9fc5b186aef9db21dbc542d06a7bd6e314be3a30
FDS1990 <f.disales@hotmail.com>
2018-03-27 19:38:40 +0200

    File Kotlin

--- Kotlin_to_Javascript/src/Test.kt
+++ Kotlin_to_Javascript/src/Test.kt
@@ -1,6 +1,25 @@
-fun main()
+import org.w3c.dom.HTMLInputElement
+import kotlin.browser.*
+
+import kotlin.js.*
+
+
+fun main(args:Array<String>)
 {
 
-    js("alert('Esempio codice Kotlin tradotto in Javascript')")
+   println("hello world")
+
+}
+
+fun myFunction()
+{
+
+    val number_1= document.getElementById("number_1txt") as HTMLInputElement
+    val number_2= document.getElementById("number_2txt") as HTMLInputElement
+
+
+    val somma= number_1.value.toInt()+number_2.value.toInt()
+
+    js("alert(somma)")
 
 }
