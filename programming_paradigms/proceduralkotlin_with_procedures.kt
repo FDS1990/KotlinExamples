55d4ea8d1cbe48e76863ee1e96da860ab66f9771
FDS1990 <f.disales@hotmail.com>
2018-04-08 19:00:00 +0200

    Create proceduralkotlin_with_procedures.kt

---
+++ programming_paradigms/proceduralkotlin_with_procedures.kt
@@ -0,0 +1,19 @@
+
+
+fun hello():Unit
+{
+    
+    println("hello")
+    
+}
+
+fun hello_again()
+{
+    println("hello again")
+}
+
+fun main(args: Array<String>) 
+{
+    hello()
+    hello_again()
+}
