1373358260bfe6892ca2d6be60ccf8a4b13725e4
FDS1990 <f.disales@hotmail.com>
2018-04-08 18:27:22 +0200

    Create OOP_primaryconstructor.kt

---
+++ programming_paradigms/OOP_primaryconstructor.kt
@@ -0,0 +1,32 @@
+
+class calcolatrice(var a:Int,var b:Int)
+{
+    
+	fun somma():Int
+	{
+    
+    	val risultato=a+b
+    	return risultato
+    
+	}
+
+	fun differenza():Int
+	{
+    
+    	val risultato=a-b
+    	return risultato
+    
+	}
+    
+}
+
+
+fun main(args: Array<String>) 
+{
+    
+    val risultato=calcolatrice(2,2)
+    
+    println(risultato.somma())
+    println(risultato.differenza())
+    
+}
