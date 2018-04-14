5846549ef923ee47d2956884b3467cccc52ff314
FDS1990 <f.disales@hotmail.com>
2018-04-14 16:58:55 +0200

    Create Calculator.kt

---
+++ JavaFXvsTornadoFX/Calculator.kt
@@ -0,0 +1,18 @@
+import javafx.scene.control.Button
+import javafx.scene.layout.VBox
+import javafx.scene.paint.Color
+import tornadofx.*
+
+class MyView : View() {
+
+    override val root = VBox()
+
+    init {
+        with(root) {
+            this += Button("Press Me").apply {
+                textFill = Color.RED
+                action { println("Button pressed!") }
+            }
+        }
+    }
+}
