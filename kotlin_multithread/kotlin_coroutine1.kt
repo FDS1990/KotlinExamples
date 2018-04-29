7d27dcfe44549df535b4eb0df3911263036717fe
FDS1990 <f.disales@hotmail.com>
2018-04-29 17:54:52 +0200

    Create kotlin_coroutine1.kt

---
+++ kotlin_multithread/kotlin_coroutine1.kt
@@ -0,0 +1,22 @@
+
+import kotlinx.coroutines.experimental.*
+
+fun main(args: Array<String>)
+{
+
+    println("Kotlin - test coroutine")
+    
+ launch 
+ {
+ 
+    delay(2000)
+    println("Kotlin - istruzione coroutine")
+ 
+ }
+ 
+    println("Kotlin - end test coroutine")
+}
+
+
+
+
